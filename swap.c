d03f64778de05d4fa860ce434059d13d05f3f226
Jasleen74 <preetjee@gmail.com>
2025-10-17 22:46:41 +0530

    The function to swap the values

---
+++ swap.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+void swap (int *a , int *b ){
+    int temp ;
+    temp = *a;
+    *a = *b ;
+    *b = temp;
+}
+int main() {
+    int a = 7;
+    int b = 8;
+    swap(&a,&b);
+    printf("the value of a is :%d  and the value of b is :%d", a , b );
+     
+  
+ return 0;
+}
