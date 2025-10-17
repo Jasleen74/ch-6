448f26813d228f51a5451069c61b3f0c34aa56b6
Jasleen74 <preetjee@gmail.com>
2025-10-17 11:46:43 +0530

    The adress of the integer

---
+++ pointers.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+     
+     int b = 112;
+     printf("The address of the a is %p\n",&b); // we will always use this
+     printf("The address of a in integer is %u",&b); // unsigned integer
+  
+ return 0;
+}
